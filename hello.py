9ab34b0588cc5ca62a8323741295b0bee2a541c3
john-graves-les-mills <john.graves@lesmills.com>
2022-12-14 11:29:14 +1300

    20221214 1129

--- hello.py
+++ hello.py
@@ -1,27 +1,17 @@
 import streamlit as st
 import snowflake.connector
 
-def init_connection():
-    return snowflake.connector.connect(
+conn_sflake = snowflake.connector.connect(
         **st.secrets["snowflake"], client_session_keep_alive=True
     )
-conn = init_connection()
+snowflake_cursor = conn_sflake.cursor() 
+
 # Perform query.
-# Uses st.experimental_memo to only rerun when the query changes or after 10 min.
-@st.experimental_memo(ttl=600)
 def run_query(query):
-    with conn.cursor() as cur:
-        cur.execute(query)
-        return cur.fetchall()
+    query_pandas = snowflake_cursor.execute(query).fetch_pandas_all()
+    return query_pandas
 
-# Uses st.experimental_memo to only rerun when the query changes or after 10 min.
-@st.experimental_memo(ttl=600)
-def query(query):
-    with conn.cursor() as cur:
-        cur.execute(query)
-        return cur.fetch_pandas_all()
-
-my_variable = query("SELECT * FROM LMI_TEST.APPFIGURES.STREAMLIT_20221214")
+my_variable = run_query("SELECT * FROM LMI_TEST.APPFIGURES.STREAMLIT_20221214")
 my_df = st.dataframe(my_variable)
 st.write(f"Connected to Snowflake with {len(my_variable)} rows")
 
