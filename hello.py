6779e555bc2bca05828a543bd36ad7ffe0f8f57c
john-graves-les-mills <john.graves@lesmills.com>
2022-12-14 12:43:08 +1300

    20221214 1243

--- hello.py
+++ hello.py
@@ -1,28 +1,22 @@
-import pandas as pd
 import streamlit as st
 import snowflake.connector
-
 from st_aggrid import GridOptionsBuilder, AgGrid, GridUpdateMode, DataReturnMode
 
-conn_sflake = snowflake.connector.connect(
+def init_connection():
+    return snowflake.connector.connect(
         **st.secrets["snowflake"], client_session_keep_alive=True
     )
-snowflake_cursor = conn_sflake.cursor() 
-
+conn = init_connection()
 # Perform query.
-# Cache the dataframe so it's only loaded once
-@st.experimental_memo
+# Uses st.experimental_memo to only rerun when the query changes or after 10 min.
+@st.experimental_memo(ttl=600)
 def run_query(query):
-    query_pandas = snowflake_cursor.execute(query).fetch_pandas_all()
-    return query_pandas
+    with conn.cursor() as cur:
+        cur.execute(query)
+        return cur.fetch_pandas_all()
 
-# Boolean to resize the dataframe, stored as a session state variable
-# st.checkbox("Use container width", value=False, key="use_container_width")
 
-df = run_query("SELECT * FROM LMI_TEST.APPFIGURES.STREAMLIT_20221214 LIMIT 10")
+my_df = run_query("SELECT * FROM LMI_TEST.APPFIGURES.STREAMLIT_20221214")
+st.write(my_df.to_html())
 
-# Display the dataframe and allow the user to stretch the dataframe
-# across the full width of the container, based on the checkbox value
-# st.dataframe(df, use_container_width=st.session_state.use_container_width)
-# AgGrid(df)
-st.write(df.to_html())
+AgGrid(my_df)
