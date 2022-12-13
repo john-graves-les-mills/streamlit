88e0ea9ac6666c42b6a45881823dae0becfc09d6
John Graves <120435305+john-graves-les-mills@users.noreply.github.com>
2022-12-14 10:00:01 +1300

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,4 +1,24 @@
 import streamlit as st
+
+def init_connection():
+    return snowflake.connector.connect(
+        **st.secrets["snowflake"], client_session_keep_alive=True
+    )
+conn = init_connection()
+# Perform query.
+# Uses st.experimental_memo to only rerun when the query changes or after 10 min.
+@st.experimental_memo(ttl=600)
+def run_query(query):
+    with conn.cursor() as cur:
+        cur.execute(query)
+        return cur.fetchall()
+
+my_variable = run_query("SELECT * FROM LMI_TEST.APPFIGURES.STAGE_SALESFORCE_STREAMLIT_TARGET")
+st.dataframe(my_variable)
+my_df = st.dataframe(my_variable)
+st.write(f"Connected to Snowflake with {len(my_variable)} rows")
+
+                        
 if st.button("Click me"):
   st.write("Hello world")
 
