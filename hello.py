7830fc63a91bf462719cc8ec8aed3a2cc4b4d959
john-graves-les-mills <john.graves@lesmills.com>
2022-12-15 12:11:32 +1300

    20221215 1211 Added Base and Premium split

--- hello.py
+++ hello.py
@@ -26,21 +26,33 @@ def t(title_string, no_year=False, silent=False):
 conn_sflake = snowflake.connector.connect(**st.secrets["snowflake"], client_session_keep_alive=True)
 snowflake_cursor = conn_sflake.cursor()   
 st.header("Year on Year Daily Acquisitions")
-df = run_query(f"SELECT * FROM LMI_TEST.APPFIGURES.STREAMLIT_20221214")
-st.write(f"Fetched {len(df)} Days of Data through {max(df.CREATED_DATE)}")
-df2021 = df[(df.CREATED_DATE >= datetime.date(2021,11,25)) & (df.CREATED_DATE <= datetime.date(2022,1,25))].copy()
+df = run_query(f"""SELECT * FROM LMI_TEST.APPFIGURES.DAILY_ACQUISITIONS_20221215""")
+
+st.subheader(f"LM+ Acquisitions 2021 v 2022 (to {max(df.CREATED_DATE)})")
+st.write(f"Fetched Data from {min(df.CREATED_DATE)} to {max(df.CREATED_DATE)} and shifted 2022 data back 364 days to align with 2021")
+st.write(f"Tip: 2022 Columns are split by Base and Premium Tier - hover mouse on bars to see separate counts")
+
+# 2021
+df2021 = df[(df.CREATED_DATE >= datetime.date(2021,11,25)) & 
+            (df.CREATED_DATE <= datetime.date(2022,1,25)) &
+            (df.TIER == 'TIER#premium')].copy()
+df2021.rename(columns={'ROW_COUNT':'2021'}, inplace=True)
+
+# 2022
 df2022 = df[(df.CREATED_DATE >= datetime.date(2022,11,25)) & (df.CREATED_DATE <= datetime.date(2023,1,25))].copy()
 # datetime.date(2022,11,29) - datetime.date(2021,11,30) # 364 days
 df2022['CREATED_DATE'] = df2022['CREATED_DATE'] - datetime.timedelta(days=364)
-df2022.rename(columns={'ROW_COUNT':'2022'}, inplace=True)
-df2021.rename(columns={'ROW_COUNT':'2021'}, inplace=True)
-df2 = df2021[['CREATED_DATE', '2021']].merge(df2022[['CREATED_DATE', '2022']], how='left')
-title = t(f"Acquisitions 2021 v 2022 (N={len(df2):,})")
+df2022b = df2022.pivot(index='CREATED_DATE' , columns='TIER' , values='ROW_COUNT')
+col_map = {'TIER#base':'2022 Base', 'TIER#premium':'2022 Premium'}
+df2022b.columns = [col_map[col] for col in list(df2022b.columns)]
+df2022b.reset_index(inplace=True)
+df2 = df2021[['CREATED_DATE', '2021']].merge(df2022b, how='left')
+df3 = pd.melt(df2, id_vars='CREATED_DATE', value_vars=['2021', '2022 Base', '2022 Premium'])
+df3['Year'] = df3.variable.apply(lambda row: row[:4])
 
-fig = px.bar(df2, x=df2.columns[0], y=df2.columns[1:], width=940)
-fig.update_layout(title=title,
-                 xaxis_title='Created Date (minus 1 year for 2022)',
-                 yaxis_title='Count',
+fig = px.bar(df3, x=df3.columns[0], y=df3.columns[2], width=940, hover_name='variable', color='Year')
+fig.update_layout(xaxis_title='Created Date (minus 1 year for 2022)',
+                  yaxis_title='Count',
                   legend_title_text='Year',
-                 barmode='group')
+                  barmode='group')
 st.plotly_chart(fig)
