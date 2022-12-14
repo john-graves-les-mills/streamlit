a2ae56179916a3e65f9a1522b64d78db83a7a58f
john-graves-les-mills <john.graves@lesmills.com>
2022-12-14 14:00:50 +1300

    20221214 1400

--- hello.py
+++ hello.py
@@ -1,7 +1,6 @@
 import pandas as pd
 import streamlit as st
 import snowflake.connector
-from st_aggrid import GridOptionsBuilder, AgGrid, GridUpdateMode, DataReturnMode
 
 def init_connection():
     return snowflake.connector.connect(
@@ -20,7 +19,5 @@ def run_query(query):
 data = run_query("SELECT * FROM LMI_TEST.APPFIGURES.STREAMLIT_20221214 LIMIT 10")
 df = pd.DataFrame(data)
 st.write(f"Connected to Snowflake with {len(df)} rows")
-st.write(str(type(df)))
-st.write(str(df.columns)) # AttributeError: 'list' object has no attribute 'columns'
-df.columns = ['A', 'B', 'C']
-AgGrid(df)
+df.columns = ['CREATED_DATE','ROW_COUNT','VIMEO_ID_COUNT']
+st.dataframe(df)
