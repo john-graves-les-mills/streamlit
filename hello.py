79f9370b0f2a864d3f50f64bd62aef6082e06b9f
john-graves-les-mills <john.graves@lesmills.com>
2022-12-14 16:18:43 +1300

    20221214 1618 First plotly fig

--- hello.py
+++ hello.py
@@ -14,5 +14,11 @@ def run_query(query):
 conn_sflake = snowflake.connector.connect(**st.secrets["snowflake"], client_session_keep_alive=True)
 snowflake_cursor = conn_sflake.cursor()   
 st.write("20221214 1609 ")
-query1 = run_query(f"select distinct SALESPERSON from LMI_TEST.APPFIGURES.STAGE_SALESFORCE_STREAMLIT_TARGET")
-st.write(query1)
+df = run_query(f"SELECT * FROM LMI_TEST.APPFIGURES.STREAMLIT_20221214 LIMIT 10")
+st.write(df)
+
+fig = px.bar(df.head(10), x=df.columns[0], y='ROW_COUNT', width=940)
+fig.update_layout(title="Acquisitions",
+    xaxis_title='Date',
+    yaxis_title='Count') #,legend_title_text='')
+st.plotly_chart(fig)
