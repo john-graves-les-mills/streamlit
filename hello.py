5b524b97f221520fd612149829efd113bf208a11
john-graves-les-mills <john.graves@lesmills.com>
2022-12-14 15:25:44 +1300

    20221214 1525

--- hello.py
+++ hello.py
@@ -11,8 +11,10 @@ def run_query(query):
 
 
 
+
 conn_sflake = snowflake.connector.connect(**st.secrets["snowflake"], client_session_keep_alive=True)
 snowflake_cursor = conn_sflake.cursor()   
 query1 = run_query(f"select distinct SALESPERSON from LMI_TEST.APPFIGURES.STAGE_SALESFORCE_STREAMLIT_TARGET")
 st.write(query1)
 exit()
+# 20221214 1525 
