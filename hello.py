d7cd23cd2fbe1caedebcfadfcea899ade2c99fb4
john-graves-les-mills <john.graves@lesmills.com>
2022-12-14 15:04:08 +1300

    20221214 1504

--- hello.py
+++ hello.py
@@ -3,13 +3,17 @@ import datetime
 import pandas as pd
 import snowflake.connector
 
+
+
 def run_query(query):
     query_pandas = snowflake_cursor.execute(query).fetch_pandas_all()
     return query_pandas
 
+
+
+
 conn_sflake = snowflake.connector.connect(**st.secrets["snowflake"], client_session_keep_alive=True)
 snowflake_cursor = conn_sflake.cursor()   
-df = run_query(f"SELECT * FROM LMI_TEST.APPFIGURES.STREAMLIT_20221214 LIMIT 10")
-st.write(str(type(df)))
-st.write(df)
+query1 = run_query(f"select distinct SALESPERSON from LMI_TEST.APPFIGURES.STAGE_SALESFORCE_STREAMLIT_TARGET")
+st.write(query1)
 exit()
