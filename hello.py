b76766831946eb6333c0a70fa5d241e189c8e63f
john-graves-les-mills <john.graves@lesmills.com>
2022-12-14 13:22:09 +1300

    20221214 1322

--- hello.py
+++ hello.py
@@ -15,14 +15,6 @@ def run_query(query):
         return cur.fetch_pandas_all()
 
 
-my_variable = run_query("SELECT * FROM LMI_TEST.APPFIGURES.STREAMLIT_20221214 LIMIT 10")
-my_df = st.dataframe(my_variable)
-st.write(f"Connected to Snowflake with {len(my_variable)} rows")
-
-                        
-if st.button("Click me"):
-  st.write("Hello world 20221214 1113")
-
-my_pick = st.text_input("Pick a number:")
-if my_pick:
-  st.write(f"You picked: {my_pick}")
+df = run_query("SELECT * FROM LMI_TEST.APPFIGURES.STREAMLIT_20221214 LIMIT 10")
+st.write(f"Connected to Snowflake with {len(df)} rows")
+st.write(str(df.columns))
