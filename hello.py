724218e8c99193434ef25eda5eb4dd1c32c64ce7
john-graves-les-mills <john.graves@lesmills.com>
2022-12-14 13:35:39 +1300

    20221214 1335

--- hello.py
+++ hello.py
@@ -12,9 +12,9 @@ conn = init_connection()
 def run_query(query):
     with conn.cursor() as cur:
         cur.execute(query)
-        return cur.fetchall()
+        return cur.fetchall() # Returns a list of lists
 
 
 df = run_query("SELECT * FROM LMI_TEST.APPFIGURES.STREAMLIT_20221214 LIMIT 10")
 st.write(f"Connected to Snowflake with {len(df)} rows")
-st.write(str(df.columns))
+# st.write(str(df.columns)) # AttributeError: 'list' object has no attribute 'columns'
